75f084ede8feb8dd9ef2f51967114c0ebed02bc5
Michael Jordan <michael@glowingpixel.com>
2023-08-31 20:19:52 -0700

    Better-ish

--- examples/get_continuity.py
+++ examples/get_continuity.py
@@ -1,95 +1,167 @@
 import sys, pathlib
 import avb
 import trtlib
-from timecode import Timecode
+from timecode import Timecode, TimecodeRange
 
 USAGE = f"Usage: {__file__} path/to/bins"
 
-def get_video_track_from_sequence(seq:avb.trackgroups.Composition, track_index:int=1) -> avb.components.Sequence:
+"""
+
+Notes:
+
+Groupclip?  Selected Track -> Component
+
+Trackgroup? Track (media_kind=picture) -> Component
+
+SourceClip? Lookup MobID
+
+Composition? GOTO Trackgroup, or usagecode is masterclip?
+
+Sequence?  Get the list of components (may contain filler, source clips, or something else)
+
+"""
+
+def matchback_groupclip(group:avb.trackgroups.Selector, selected_track_index:int|None=None) -> avb.components.Component:
+	"""Matchback a groupclip"""
+
+	# Groupclip seems to be a TrackGroup with a `selected` property pointing to the active camera
+	selected_track = group.tracks[selected_track_index if selected_track_index is not None else group.selected]
+	return matchback_track(selected_track)
+
+def matchback_trackgroup(track_group:avb.trackgroups.TrackGroup, media_kind:str="picture") -> avb.components.Component:
+
+	filtered_tracks = [t for t in track_group.tracks if t.media_kind == media_kind]
+
+	if len(filtered_tracks) != 1:
+		return f"{track_group}: Got {filtered_tracks}"
+
+	return matchback_track(filtered_tracks[0])
+
+def matchback_track(track:avb.trackgroups.Track) -> avb.components.Component:
+	"""Get the component of a Track"""
+
+	return track.component
+
+def matchback_sequence(sequence:avb.components.Sequence) -> avb.components.Component:
+	"""Get components from a sequence"""
+	
+	# Sequences start and end with filler
+	# Probably want to do a "get component at location" thing here
+
+	if len(sequence.components) > 3:
+		return f"Sequence has {len(sequence.components)} components: {sequence.components}"
+
+	#print(f" Returning {sequence.components[1]}")
+	return sequence.components[1]
+
+def matchback_sourceclip(source_clip:avb.components.SourceClip, bin_handle:avb.file.AVBFile) -> avb.components.Component:
+	"""Resolve MOB for a given `SourceClip`"""
+
+	return bin_handle.content.find_by_mob_id(source_clip.mob_id)
+
+def get_video_track_from_composition(composition:avb.trackgroups.Composition, media_kind:str="picture", track_index:int=1) -> avb.components.Sequence:
 	"""Get V1 by default"""
 
-	for track in seq.tracks:
-		if track.index == 1 and track.media_kind == "picture":
+	for track in composition.tracks:
+		if track.index == track_index and track.media_kind == media_kind:
 			return track.component
 	
 	raise IndexError(f"No V{track_index} found in sequence")
 
 
+def is_masterclip(component:avb.components.Component) -> bool:
+	"""Is a component a masterclip?"""
+	
+	return isinstance(component, avb.trackgroups.Composition) and component.mob_type == "MasterMob"
+
+
+def print_masterclip_info(masterclip:avb.trackgroups.Composition, duration:TimecodeRange):
+	"""Display info (temp?)"""
+
+	scene_number = masterclip.attributes.get("_USER").get("Scene")
+	take_number  = masterclip.attributes.get("_USER").get("Take")
+	tape_name    = masterclip.attributes.get("_USER").get("TapeID")
+	clip_name    = masterclip.name
+	
+	#sys.exit(daily_video_track_component_mob.name)
+	print(f"SRCCLP:\t{duration.start}\tName: {str(clip_name).ljust(24)} Sc: {str(scene_number).ljust(16)} Tk: {str(take_number).ljust(8)} Tape: {str(tape_name).ljust(24)} Dur: {duration.duration}")
+
+
+
 def do_bin_good_and_nice(bin_path:str):
 
 	with avb.open(bin_path) as bin_handle:
-		latest_sequence:avb.trackgroups.Composition = sorted(trtlib.get_sequences_from_bin(bin_handle.content), key=lambda x: trtlib.human_sort(x.name))[0]
+		latest_sequence:avb.trackgroups.Composition = sorted(trtlib.get_sequences_from_bin(bin_handle.content), key=lambda x: trtlib.human_sort(x.name))[-1]
 		
 		print(f"{latest_sequence.name} has {len(latest_sequence.tracks)} track(s)")
 
-		track_v1 = get_video_track_from_sequence(latest_sequence)
+		# "Avid Sequence" is a Composition which contains tracks, each with a sequence component
+		# So V1 would be the Sequence component of Track 1 Picture
+		track_v1 = get_video_track_from_composition(latest_sequence)
 
-		# For Now: Just need Selectors (group clips)?
-		groupclips = [x for x in track_v1.components if isinstance(x, avb.trackgroups.Selector)]
-		print(f"V1 has {len(groupclips)} groupclips")
+		tc_current = Timecode("02:00:00:00", rate=round(track_v1.edit_rate))
 
-		for groupclip in groupclips:
+		for component in track_v1.components:
 
-			length = groupclip.length
-			edit_rate = groupclip.edit_rate
+			# Get the length of the most-parent-y component as it lives in the timeline
+			og_rate   = component.edit_rate
+			og_length = component.length
 
-			# Group clip contains multiple Tracks and a "Selected" index for the one in use
-			# Each track contains a component with a Mob ID that references subclips of the component masterclips
-			# We look up the mob ID using the bin's find_by_mob_id() which gives us the Composition (master clip?? or maybe this is a subclip of a group_subclip... that might be what's going on)
+			if og_length == 0:
+				#print(f"skip {component}")
+				continue
 
-			# Get the selected group track (camera A, camera B)
-			group_selected_track:avb.trackgroups.Track = groupclip.tracks[groupclip.selected]
+			# Match back until we have the masterclip
+			while not is_masterclip(component):
 
-			# Get the component of that track, which is a `SourceClip` which will point to a subclip of a masterclip and any filler
-			group_selected_sourceclip:avb.components.SourceClip = group_selected_track.component
+				if isinstance(component, avb.trackgroups.Composition):
+					component = get_video_track_from_composition(component)
 
-			# Look up the `mob_id`` to get a reference to the actual subclip composition
-			try:
-				group_subclip_composition:avb.trackgroups.Composition = bin_handle.content.find_by_mob_id(group_selected_sourceclip.mob_id)
-			except Exception as e:
-				print(f"** Skip {groupclip}: {e}")
+				if isinstance(component, avb.trackgroups.Selector):
+					component = matchback_groupclip(component)
+
+				elif isinstance(component, avb.trackgroups.TrackGroup):
+					component = matchback_trackgroup(component)
+				
+				# Oooohh.... you know whaaaat.....
+				elif isinstance(component, avb.trackgroups.Track):
+					component = matchback_track(component)
+				
+				elif isinstance(component, avb.components.SourceClip):
+					component = matchback_sourceclip(component, bin_handle)
+
+				elif isinstance(component, avb.components.Sequence):
+					component = matchback_sequence(component)
+
+				elif isinstance(component, avb.components.Filler):
+					break
+				
+				else:
+					break
+
+				#print(component)
+				#input()
+			
+			#print(f"!!! GOT MASTERCLIP {component}")
+
+			tc_subclip = TimecodeRange(
+				start=tc_current,
+				duration=Timecode(og_length, rate=round(og_rate))
+			)
+
+			if isinstance(component, avb.components.Filler):
+				print(f"Filler {component.length}")
+				tc_current += og_length
+				continue
+
+			elif not isinstance(component, avb.trackgroups.Composition):
+				print(f"**SKIP: {tc_subclip.start}\t{component} ]]")
+				tc_current += og_length
 				continue
 			
-			# group_subclip Composition contains tracks
-			# Picture tracks have a component of type "Sequence".
-			# Here I'm collecting the sequence from each picture track in the group_subclip
-			group_subclip_composition_tracks:list[avb.trackgroups.Track] = group_subclip_composition.tracks
-			
-			# Going go grab just the first picture track?
-			group_subclip_composition_picture_track:avb.trackgroups.Track = [t for t in group_subclip_composition_tracks if t.media_kind == "picture"][0]
+			print_masterclip_info(component, duration=tc_subclip)
+			tc_current += tc_subclip.duration
 
-			group_subclip_composition_picture_sequence:avb.components.Sequence = group_subclip_composition_picture_track.component
-
-			# Picture track component is a Sequence.
-			# The Sequence has a list of source components
-			# I've seen Filler and Source Clips here
-			group_subclip_composition_picture_components:list[avb.utils.AVBObjectRef] = group_subclip_composition_picture_sequence.components
-			
-			# Filter out the dailies footage which is a `SourceClip` (Rather than Filler)
-			group_subclip_composition_picture_daily:list[avb.components.SourceClip] = [c for c in group_subclip_composition_picture_components if isinstance(c, avb.components.SourceClip)][0]
-			
-			# Get the MOB ID of that daily
-			daily_mob_id = group_subclip_composition_picture_daily.mob_id
-
-			# This is the Master Clip (autosync of video and audio), I believe
-			daily_mob:avb.trackgroups.Composition = bin_handle.content.find_by_mob_id(daily_mob_id)
-
-			# Get the video of the sync clip
-			daily_video_track = [t for t in daily_mob.tracks if t.media_kind == "picture"][0]
-			
-			# Get the source clip component of the video
-			daily_video_track_component:avb.components.SourceClip = daily_video_track.component
-
-			# This is the master mob!!!!!!!
-			daily_video_track_component_mob:avb.trackgroups.Composition = bin_handle.content.find_by_mob_id(daily_video_track_component.mob_id)
-
-			# Get the Scene!
-			scene_number = daily_video_track_component_mob.attributes.get("_USER").get("Scene")
-			take_number  = daily_video_track_component_mob.attributes.get("_USER").get("Take")
-			tape_name    = daily_video_track_component_mob.attributes.get("_USER").get("TapeID")
-			
-			#sys.exit(daily_video_track_component_mob.name)
-			print(f"Scn {scene_number}, Tk {take_number} ({tape_name})    -   {Timecode(length, rate=int(edit_rate))}")
 			
 
 		
