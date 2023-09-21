8877db7bd4baf03b477606967924928026fd5628
mjiggidy <michael@glowingpixel.com>
2023-09-20 21:26:39 -0700

    Better matchbacks

--- avbutils/timeline.py
+++ avbutils/timeline.py
@@ -8,3 +8,11 @@ def get_timelines_from_bin(bin:avb.bin.Bin) -> collections.abc.Generator[avb.tra
 	"""Get all top-level timelines ("Sequences" in Media Composer) in a given Avid bin"""
 	return (mob for mob in bin.toplevel() if isinstance(mob, avb.trackgroups.Composition))
 
+def get_video_track_from_composition(composition:avb.trackgroups.Composition, media_kind:str="picture", track_index:int=1) -> avb.components.Sequence:
+	"""Get V1 by default"""
+
+	for track in composition.tracks:
+		if track.index == track_index and track.media_kind == media_kind:
+			return track.component
+	
+	raise IndexError(f"No V{track_index} found in sequence")
