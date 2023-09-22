beeaa0b0fdc9c42ef4a06a35b056fc8e358ec7bd
mjiggidy <michael@glowingpixel.com>
2023-09-21 19:27:55 -0700

    Understanding a little more each day...

--- examples/get_continuity_from_v1.py
+++ examples/get_continuity_from_v1.py
@@ -5,27 +5,6 @@ from timecode import Timecode, TimecodeRange
 
 USAGE = f"Usage: {__file__} path/to/bins"
 
-"""
-
-Notes:
-
-Groupclip?  Selected Track -> Component
-
-Trackgroup? Track (media_kind=picture) -> Component
-
-SourceClip? Lookup MobID
-
-Composition? GOTO Trackgroup, or usagecode is masterclip?
-
-Sequence?  Get the list of components (may contain filler, source clips, or something else)
-
-"""
-
-
-
-
-
-
 def print_masterclip_info(masterclip:avb.trackgroups.Composition, duration:TimecodeRange, kind:str):
 	"""Display info (temp?)"""
 
@@ -42,83 +21,81 @@ def print_masterclip_info(masterclip:avb.trackgroups.Composition, duration:Timec
 def do_bin_good_and_nice(bin_path:str):
 
 	with avb.open(bin_path) as bin_handle:
-		latest_sequence:avb.trackgroups.Composition = sorted(avbutils.get_timelines_from_bin(bin_handle.content), key=lambda x: avbutils.human_sort(x.name))[-1]
+		latest_timeline:avb.trackgroups.Composition = sorted(avbutils.get_timelines_from_bin(bin_handle.content), key=lambda x: avbutils.human_sort(x.name))[-1]
 		
-		print(f"{latest_sequence.name} has {len(latest_sequence.tracks)} track(s)")
+		print(f"{latest_timeline.name} has {len(latest_timeline.tracks)} track(s)")
 
 		# "Avid Sequence" is a Composition which contains tracks, each with a sequence component
 		# So V1 would be the Sequence component of Track 1 Picture
-		track_v1 = get_video_track_from_composition(latest_sequence)
 
-		tc_current = Timecode("02:00:00:00", rate=round(track_v1.edit_rate))
+		try:
+			track_v1:avb.trackgroups.Track = list(avbutils.get_tracks_from_composition(latest_timeline, type=avbutils.TrackTypes.PICTURE, index=1))[0]
+		except IndexError:
+			raise RuntimeError(f"{latest_timeline.name}: Track V1 was expected, but not found.", file=sys.stderr)
+		
+		# With the desired track, get the track's *component*, which should be a linear avb.components.Sequence of all the clips in the track
+		sequence_v1 = track_v1.component
+		if not isinstance(sequence_v1, avb.components.Sequence):
+			raise ValueError(f"{track_v1}: Expected a sequence, but got {sequence_v1}")
+		
+		tc_current = Timecode("02:00:00:00", rate=round(sequence_v1.edit_rate))
 
-		for component in track_v1.components:
+		for subclip in sequence_v1.components:
 
-			# Get the length of the most-parent-y component as it lives in the timeline
-			og_class  = component.class_id.decode()
-			og_rate   = component.edit_rate
-			og_length = component.length
-
-			if og_length == 0:
+			# Sequences always start and end with zero-length filler we can ignore
+			if subclip.length == 0:
 				#print(f"skip {component}")
 				continue
 
-			# Match back until we have the masterclip
-			while not avbutils.is_masterclip(component):
-
-				if isinstance(component, avb.trackgroups.Composition):
-					component = get_video_track_from_composition(component)
-
-				if isinstance(component, avb.trackgroups.Selector):
-					component = avbutils.matchback_groupclip(component)
-
-				elif isinstance(component, avb.trackgroups.TrackGroup):
-					component = avbutils.matchback_trackgroup(component)
-				
-				# Oooohh.... you know whaaaat.....
-				elif isinstance(component, avb.trackgroups.Track):
-					component = avbutils.matchback_track(component)
-				
-				elif isinstance(component, avb.components.SourceClip):
-					component = avbutils.matchback_sourceclip(component, bin_handle)
-
-				elif isinstance(component, avb.components.Sequence):
-					component = avbutils.matchback_sequence(component)
-
-				elif isinstance(component, avb.components.Filler):
-					break
-				
-				else:
-					break
-
-				#print(component)
-				#input()
-			
-			#print(f"!!! GOT MASTERCLIP {component}")
-
-			tc_subclip = TimecodeRange(
+			# Here's the "record TC" based on how far along we are in the sequence, and the subclip's length
+			tc_record = TimecodeRange(
 				start=tc_current,
-				duration=Timecode(og_length, rate=round(og_rate))
+				duration=Timecode(subclip.length, rate=round(subclip.edit_rate))
 			)
 
-			if isinstance(component, avb.components.Filler):
-				print(f"Filler {component.length}")
-				tc_current += og_length
-				continue
+			# SKIPS BEFORE MATCHBACK
 
-			elif not isinstance(component, avb.trackgroups.Composition):
-				print(f"**SKIP: {tc_subclip.start}\t{component} ]]")
-				tc_current += og_length
+			# Transitions
+			# Transitions overlap the *previous* clip in the sequence, indicating we move back (to the left in the timeline) the duration of the sequence and begin the *next* clip *there*
+			# So, also worth knowing, the NEXT subclip will have a duration of it's "usual" in/out points, *plus* the transition on the beginning of it
+			# ...if that makes any sense.
+			if isinstance(subclip, avb.trackgroups.TransitionEffect):
+				print(f"{subclip.class_id.decode()}:\tTransition Effect ({tc_record.duration} {subclip.left_length=}\t {subclip.right_length=} {subclip.cutpoint=})")
+				# TODO: CHECK ALL THIS REAL GOOD
+				tc_current -= tc_record.duration
 				continue
 			
-			print_masterclip_info(component, duration=tc_subclip, kind=og_class)
-			tc_current += tc_subclip.duration
+			# Filler
+			# Can't matchback this fella anyway
+			# TODO: Maybe pass this through in `avbutils.matchback_to_masterclip()`
+			elif isinstance(subclip, avb.components.Filler):
+				print(f"{subclip.class_id.decode()}:\t{tc_record.start}   ({tc_record.duration})")
+				tc_current += tc_record.duration
+				continue
 
+			# NOTE: For now, handling things that `avbutils.matchback_to_masterclip()` gave up on
+			if isinstance(subclip, avb.trackgroups.TrackEffect):
+				print(f"{subclip.class_id.decode()}:\t{tc_record.start} ({tc_record.duration})\t{subclip}")
+				tc_current += tc_record.duration
+				continue
+			
+
+			# Match back until we have the masterclip
+			masterclip = avbutils.matchback_to_masterclip(subclip)
+			try:
+				print_masterclip_info(masterclip, duration=tc_record, kind=subclip.class_id.decode())
+			except Exception as e:
+				print(f"{masterclip}: {e}")
+
+			# Print the info
+			tc_current += tc_record.duration
+
+	print(f"End of reel: {tc_current}")
 			
 
 		
 if __name__ == "__main__":
-    
+
 	if not len(sys.argv):
 		sys.exit(USAGE)
 	
