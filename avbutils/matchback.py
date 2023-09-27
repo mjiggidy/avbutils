c3dc3169387c514d54998ba15b1a6af257484efb
mjiggidy <michael@glowingpixel.com>
2023-09-26 20:33:04 -0700

    More matchback helpers

--- avbutils/matchback.py
+++ avbutils/matchback.py
@@ -3,6 +3,9 @@
 import avb
 from .timeline import get_tracks_from_composition, TrackTypes
 
+class IsAsMatchedBackAsCanBe(StopIteration):
+	"""This is as matched back as you can get, sir"""
+
 def matchback_groupclip(group:avb.trackgroups.Selector, selected_track_index:int|None=None) -> avb.components.Component:
 	"""Matchback a groupclip"""
 
@@ -41,12 +44,62 @@ def matchback_sourceclip(source_clip:avb.components.SourceClip) -> avb.component
 
 	return source_clip.root.content.find_by_mob_id(source_clip.mob_id)
 
-	#return bin_handle.content.find_by_mob_id(source_clip.mob_id)
+	# TODO: This does weird things. Investigate:
+	#return source_clip.track
 
 def is_masterclip(component:avb.components.Component) -> bool:
 	"""Is a component a masterclip?"""
 	
 	return isinstance(component, avb.trackgroups.Composition) and component.mob_type == "MasterMob"
+
+def is_sourcemob(component:avb.components.Component) -> bool:
+	"""Is a component a source mob?"""
+
+	return isinstance(component, avb.trackgroups.Composition) and component.mob_type == "SourceMob"
+
+def matchback_component(component:avb.components.Component) -> avb.components.Component:
+	"""Generic: Matchback a given component"""
+
+	if isinstance(component, avb.trackgroups.Composition):
+		return next(get_tracks_from_composition(component, type=TrackTypes.PICTURE, index=1))
+
+	if isinstance(component, avb.trackgroups.Selector):
+		return matchback_groupclip(component)
+
+	elif isinstance(component, avb.trackgroups.TrackGroup):
+		return matchback_trackgroup(component)
+	
+	# Oooohh.... you know whaaaat.....
+	elif isinstance(component, avb.trackgroups.Track):
+		return matchback_track(component)
+	
+	elif isinstance(component, avb.components.SourceClip):
+		return matchback_sourceclip(component)
+
+	elif isinstance(component, avb.components.Sequence):
+		return matchback_sequence(component)
+	
+	else:
+		raise IsAsMatchedBackAsCanBe()
+
+def matchback_to_sourcemob(component:avb.components.Component) -> avb.components.Component:
+	"""Given a component, match it back until we're at the source mob"""
+
+	# Keep goin' 'till we can't SourceMob no more??
+	# TODO: VERY SKEPTICAL THAT THIS IS THE WAY TO GO.
+
+	source_mob = None
+
+	while True:
+		try:
+			component = matchback_component(component)
+			if is_sourcemob(component):
+				source_mob = component
+		except IsAsMatchedBackAsCanBe:
+			break
+	
+	return source_mob
+
 
 def matchback_to_masterclip(component:avb.components.Component) -> avb.components.Component:
 	"""Given a component, match it back until we're at the masterclip"""
