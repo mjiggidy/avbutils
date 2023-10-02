6b25bb74d9dbce1c0bf0c635f93674a907041854
Michael Jordan <michael@glowingpixel.com>
2023-10-02 16:54:03 -0400

    Real cool formattin thangs

--- examples/get_continuity_from_clips.py
+++ examples/get_continuity_from_clips.py
@@ -5,6 +5,7 @@ from timecode import Timecode, TimecodeRange
 USAGE = f"Usage: {__file__} path/to/bins"
 
 pat_reelname = re.compile(r"FH REEL (?P<reel_number>\d+) v(?P<reel_version>[\d\.]+)", re.I)
+pat_sequential = re.compile(r"(?P<base>.*)(?P<digits>\d+)")
 
 @dataclasses.dataclass
 class ContinuitySceneInfo:
@@ -123,9 +124,41 @@ def print_numbers_doc(reels_info:list[ReelInfo], output_path:str="out.numbers"):
 	"""Create a Numbers document for the given"""
 
 	doc = numbers_parser.Document()
+
+	# Setup styles
+	style_scene_duration = doc.add_style(
+		alignment = numbers_parser.Alignment("right")
+	)
+
+	style_scene_location = doc.add_style(
+		alignment = numbers_parser.Alignment("right"),
+		italic = True
+	)
+
+	style_reel_trt = doc.add_style(
+		alignment = numbers_parser.Alignment("right"),
+		italic = True
+	)
+
+	style_lp_label = doc.add_style(
+		bold=True
+	)
+
+	style_lp_trt = doc.add_style(
+		alignment = numbers_parser.Alignment("right"),
+		italic = True,
+		bold = True
+	)
+
+	style_blank_row = doc.add_style(
+		bg_color = numbers_parser.RGB(255,255,255)
+	)
+
 	doc.add_sheet(sheet_name="Continuity (Timings)", num_cols=5)
 	sheet = doc.sheets["Continuity (Timings)"]
 	table = sheet.tables[0]
+
+	table.num_header_cols = 0
 
 	row_num = 0
 #	table.add_row(3)
@@ -143,9 +176,15 @@ def print_numbers_doc(reels_info:list[ReelInfo], output_path:str="out.numbers"):
 		for scene_info in reel_info.scenes:
 
 			table.write(row_num, 1, scene_info.scene_number)
-			table.write(row_num, 2, str(scene_info.duration).lstrip("0:"))
+			
+			table.write(row_num, 2, str(scene_info.duration).lstrip("0:"), style=numbers_parser.Style(alignment=("right","top"), bold=True))
+			table.set_cell_style(row_num, 2, style_scene_duration)
+
 			table.write(row_num, 3, scene_info.description)
+			
 			table.write(row_num, 4, scene_info.location)
+			table.set_cell_style(row_num, 4, style_scene_location)
+
 			row_num += 1
 		
 		row_end = row_num-1
@@ -154,12 +193,41 @@ def print_numbers_doc(reels_info:list[ReelInfo], output_path:str="out.numbers"):
 		# TODO: Handle reels with scenes < reel info rows
 		table.write(row_start, 0, f"R{reel_info.reel_number} v{reel_info.reel_version}")
 		table.write(row_end-1, 0, f"R{reel_info.reel_number} TRT:")
-		table.write(row_end, 0,str(reel_info.reel_trt).lstrip("0:"))
-		#table.merge_cells([table.cell(x,0).xl_rowcol_to_cell for x in range(row_start,row_end-2)])
+		table.write(row_end, 0, str(reel_info.reel_trt).lstrip("0:"))
 		
-		row_num += 1
+		table.set_cell_style(row_end, 0, style_reel_trt)
+		
+		table.merge_cells(numbers_parser.xl_rowcol_to_cell(row_start,0)+":"+numbers_parser.xl_rowcol_to_cell(row_end-2,0))
 
-	doc.save(output_path)
+		# Draw a border around dat reel
+		#border_range = numbers_parser.xl_rowcol_to_cell(row_start, 0) + ":" + numbers_parser.xl_rowcol_to_cell(row_end, 5)
+		#table.set_cell_border(border_range, ["top","bottom","left","right"], numbers_parser.Border(2.0, numbers_parser.RGB(0,0,0), "solid"))
+		
+		# Blank row
+		row_num += 1
+		#table.set_cell_border(row_num, 0, ["left","right"], numbers_parser.Border(0.0, numbers_parser.RGB(0, 0, 0), "none"), 5)
+
+	# Write LP TRT
+	table.write(row_num-1, 0, "LP TRT:")
+	table.set_cell_style(row_num-1, 0, style_lp_label)
+	
+	table.write(row_num, 0, str(sum(reel.reel_trt for reel in reels_info)).lstrip("0:"))
+	table.set_cell_style(row_num, 0, style_lp_trt)
+
+	output_path_final = pathlib.Path(output_path)
+
+	while output_path_final.exists():
+		sequential_filename = pat_sequential.match(output_path_final.stem)
+		if sequential_filename:
+			name_base = sequential_filename.group("base")
+			name_digits = str(int(sequential_filename.group("digits"))+1).zfill(len(sequential_filename.group("digits")))
+		else:
+			name_base = output_path_final.stem
+			name_digits = "_001"
+		output_path_final = output_path_final.with_stem(name_base + name_digits)
+			
+	
+	doc.save(output_path_final)
 
 
 def get_continuity_list_for_timeline(timeline:avb.trackgroups.Composition) -> list[ContinuitySceneInfo]:
