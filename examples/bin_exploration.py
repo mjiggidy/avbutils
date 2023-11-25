9cc1a8a90f12b62e6a81cde530f28f49d1a41aa5
mjordan <michael@glowingpixel.com>
2023-11-24 20:38:10 -0800

    Into the weeds

--- examples/bin_exploration.py
+++ examples/bin_exploration.py
@@ -1,4 +1,4 @@
-import sys, enum
+import sys, enum, pprint
 import avb, avbutils
 
 class BinSortDirection(enum.IntEnum):
@@ -42,15 +42,19 @@ def show_bin_info(bin:avb.bin.Bin):
 	"""Print info from an `avb.bin.Bin`"""
 
 	print("Large bin?:  ", bin.large_bin)
-	print("View Setting:", bin.view_setting.name, bin.view_setting.kind)
+	print("Was iconic?: ", bin.was_iconic)
+	
+	print("\n-------\n")
+	
 	print("Display Mode:", avbutils.BinDisplayModes.get_mode_from_bin(bin).name)
 	print("Display Mask:", avbutils.BinDisplayOptions.get_options_from_bin(bin).name)
 	print(f"Mac font:     {bin.mac_font=} {bin.mac_font_size=}")
+	print(f"Colors (16b?):{bin.background_color=} {bin.forground_color=}")
 	print(f"Image Scale:  Frame Mode Size (4-14): {bin.mac_image_scale=} Script Mode Size (3-8): {bin.ql_image_scale=}")
 	print(f"Home rect:    (top-left, bottom-right) {bin.home_rect=}  ({bin.home_rect[3]-bin.home_rect[1]}x{bin.home_rect[2]-bin.home_rect[0]})")
-	print(f"Colors (16b?):{bin.background_color=} {bin.forground_color=}")
-	print("Was iconic?: ", bin.was_iconic)
-	print("-------")
+	
+	print("\n-------\n")
+	
 	print("Sort Columns:", [f"{c} ({BinSortDirection(d).name.title()})" for d,c in bin.sort_columns])
 
 	# Sifted items
@@ -62,6 +66,23 @@ def show_bin_info(bin:avb.bin.Bin):
 		if idx == 3:
 			print("Also...")
 		print(f" - Column \"{param.column}\" {BinSiftMethod.from_sift_item(param).name.replace('_',' ').lower()} the string \"{param.string}\"")
+
+	print("\n-------\n")
+
+	print("View Setting:", bin.view_setting.name, bin.view_setting.kind)
+	print("Kind:        ", bin.view_setting.kind) # TODO: Always "Bin View" or...?
+	
+	print("# Attributes:", bin.view_setting.attr_count)
+	print("Attr Type:   ", bin.view_setting.attr_type)
+	print("Attributes:  ", bin.view_setting.attributes.items())
+	print(f"Format Descriptors: ({len(bin.view_setting.format_descriptors)})")
+	for desc in bin.view_setting.format_descriptors:
+		pprint.pprint(desc)
+		print("-")
+	print("Columns:     ")
+	for col in bin.view_setting.columns:
+		print(" -", col)
+
 
 def show_bin_info_from_file(bin_path:str):
 	"""Given a file path to a bin, print bin info"""
