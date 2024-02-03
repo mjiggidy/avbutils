d05fa81c41053a0722dd93a442b6753311249b17
mjiggidy <michael@glowingpixel.com>
2024-02-02 22:46:00 -0800

    Big ol mesxs

--- examples/bin_item_exploration.py
+++ examples/bin_item_exploration.py
@@ -1,13 +1,44 @@
+print("In it")
 import sys
 import avb, avbutils
+from PySide6 import QtSql, QtWidgets
+
+# Database Idea:
+# Table: Bin Items
+# Table: Mobs
+
+print("Start")
+app = QtWidgets.QApplication()
+con = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+con.setDatabaseName("hey.sql")
+print("stop")
+con.open()
+print(con.isOpen())
+sys.exit(con)
 
 def list_bin(bin:avb.bin.Bin):
 	"""List the bin contents"""
 
-	for idx, item in enumerate([x for x in bin.items if x.user_placed]):
+	errors = 0
+	precomp_count = 0
+
+	for idx, item in enumerate([x for x in bin.items]):
+
+		# THOUGHTS: An "item" is its representation in the bin (position, visiblity, user-placed))
+		# And its mob is the actual object
+
 		mob = item.mob
-		item.y = 16 + (idx * 175)
-		print(mob.name, f"({item.x},{item.y})", f"Thumb Frame: {item.keyframe}")
+		#print(item, mob.name, f"({item.x},{item.y})", f"Thumb Frame: {item.keyframe}   {item.user_placed=}")
+		if "Precompute Source Mob" in str(mob):
+			precomp_count += 1
+
+		try:
+			print("*****" if avbutils.composition_is_toplevel(mob) else "", f"{mob.media_kind_id=} {mob.mc_mode=}  {avbutils.MobTypes.from_composition(mob).name} {avbutils.MobUsage.from_composition(mob).name} {mob.name}")
+		except Exception as e:
+			print(f"Chokin' because {e}: {mob}")
+			errors += 1
+
+	print(f"{errors=} {precomp_count=}")
 
 def main(path_bins):
 
@@ -15,8 +46,6 @@ def main(path_bins):
 		with avb.open(path_bin) as ref_bin:
 
 			list_bin(ref_bin.content)
-
-			ref_bin.write("out.avb")
 
 if __name__ == "__main__":
 
