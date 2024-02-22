d7474502f128fe86c0ec46a7c13f85b046b45eb4
mjiggidy <michael@glowingpixel.com>
2024-02-22 15:14:08 -0800

    progress

--- avbutils/compositions.py
+++ avbutils/compositions.py
@@ -1,4 +1,6 @@
-import enum, avb
+import enum, avb, collections
+
+ClipColor = collections.namedtuple("ClipColor", "R G B")
 
 class MobTypes(enum.IntEnum):
 	"""Mob Types"""
@@ -16,6 +18,9 @@ class MobTypes(enum.IntEnum):
 	def from_composition(cls, comp:avb.trackgroups.Composition):
 		return cls(comp.mob_type_id)
 	
+	def __str__(self) -> str:
+		"""Show name with nicer formatting"""
+		return self.name.replace("_"," ").title()
 
 class MobUsage(enum.IntEnum):
 	"""Mob Usage"""
@@ -38,7 +43,22 @@ class MobUsage(enum.IntEnum):
 	def from_composition(cls, comp:avb.trackgroups.Composition):
 		return cls(comp.usage_code)
 
+	def __str__(self) -> str:
+		"""Show name with nicer formatting"""
+		return self.name.replace("_"," ").title()
+
 def composition_is_toplevel(comp:avb.trackgroups.Composition) -> bool:
 	"""Determine if a given composition is a top-level timeline"""
 	# NOTE: Using rules from `avb.bin.Bin.toplevel()``
 	return MobTypes.from_composition(comp) == MobTypes.COMPOSITION_MOB and MobUsage.from_composition(comp) == MobUsage.UNDEFINED
+
+def composition_clip_color(comp:avb.trackgroups.Composition) -> tuple[int,int,int]|None:
+	"""Return the clip color of a composition if set"""
+
+	color_attr_fields = ("_COLOR_R", "_COLOR_G", "_COLOR_B")
+
+	attrs = comp.attributes
+	try:
+		return ClipColor(*(attrs[c] for c in color_attr_fields))
+	except KeyError:
+		return None
--- examples/binmodel.py
+++ examples/binmodel.py
@@ -1,6 +1,35 @@
-import typing, sys
-import avb, avbutils
-from PySide6 import QtCore, QtWidgets
+import typing, sys, pprint
+import avb, avbutils, timecode
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class BinItemDisplayDelegate(QtWidgets.QStyledItemDelegate):
+
+	max_8b = (1 << 8) - 1
+	max_16b = (1 << 16) - 1
+
+	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionViewItem, index:QtCore.QModelIndex|QtCore.QPersistentModelIndex) -> None:
+
+		super().paint(painter, option, index)
+		
+		mob = index.data(role=QtCore.Qt.ItemDataRole.UserRole)
+
+		if avbutils.composition_clip_color(mob) is not None:
+			
+			clip_color = QtGui.QColor(*(c/self.max_16b * self.max_8b for c in avbutils.composition_clip_color(mob)))
+			color_box = QtCore.QRect((option.rect.width()/2) - (option.rect.height()*(16/9)/2), option.rect.top(), option.rect.height()*(16/9), option.rect.height())
+
+
+			
+			brush = QtGui.QBrush()
+			brush.setColor(clip_color)
+			brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
+
+			painter.fillRect(color_box, brush)
+			brush.setColor(QtGui.QColorConstants.Black)
+			brush.setStyle(QtCore.Qt.BrushStyle.NoBrush)
+			painter.drawRect(color_box)
+			return
+
 
 class BinModel(QtCore.QAbstractItemModel):
 	"""An Avid bin!"""
@@ -55,7 +84,7 @@ class BinModel(QtCore.QAbstractItemModel):
 		return len(self._bin.view_setting.columns)
 		
 
-	def data(self, index:QtCore.QModelIndex, role:typing.Optional[int]=QtCore.Qt.DisplayRole) -> typing.Any:
+	def data(self, index:QtCore.QModelIndex, role:typing.Optional[QtCore.Qt.ItemDataRole]=QtCore.Qt.ItemDataRole.DisplayRole) -> typing.Any:
 		"""Returns the data stored under the given role for the item referred to by the index."""
 
 		if not isinstance(index, QtCore.QModelIndex) or not index.isValid():
@@ -63,11 +92,13 @@ class BinModel(QtCore.QAbstractItemModel):
 		
 		mob = index.internalPointer().mob
 
-		if role == QtCore.Qt.DisplayRole:
-			header_data = self.headerData(index.column(),QtCore.Qt.Orientation.Horizontal, QtCore.Qt.UserRole)
-			if header_data.get("type") == 201:
-				return mob.name
-			elif header_data.get("type") == 40:
+		if role == QtCore.Qt.ItemDataRole.DisplayRole:
+			header_data = self.headerData(index.column(),QtCore.Qt.Orientation.Horizontal, QtCore.Qt.ItemDataRole.UserRole)
+			if header_data.get("type") == 4:
+				return str(timecode.Timecode(mob.length)).lstrip("00:")
+			elif header_data.get("type") == 201:
+				return f"{mob.name} [{avbutils.MobTypes.from_composition(mob)} / {avbutils.MobUsage.from_composition(mob)}]"
+			elif header_data.get("type") in range(40,50):
 				if "_USER" in mob.attributes:
 					return mob.attributes.get("_USER").get(header_data.get("title"),"-")
 			elif "PreservedBinData" in mob.attributes:
@@ -80,6 +111,9 @@ class BinModel(QtCore.QAbstractItemModel):
 				else:
 					print(f"_COLUMN_{header_data.get('title').upper().replace(' ','_')}")
 		
+		elif role == QtCore.Qt.ItemDataRole.UserRole:
+			return mob
+
 		return None
 		
 
@@ -91,15 +125,37 @@ class BinModel(QtCore.QAbstractItemModel):
 		elif role == QtCore.Qt.UserRole:
 			return self._header_cache[section]
 
+def show_details(selected:QtCore.QItemSelection, deselected:QtCore.QItemSelection):
+	try:
+		idx = selected.data().indexes()[0]
+	except:
+		print("Empty selection")
+		return False
+	
+	mob = idx.model().data(idx, QtCore.Qt.ItemDataRole.UserRole)
+	#pprint.pprint(mob.attributes)
+
+	pprint.pprint(mob.attributes)
+	
+
 
 if __name__ == "__main__":
 
 	app = QtWidgets.QApplication()
 	wdg = QtWidgets.QTreeView()
 	wdg.setAlternatingRowColors(True)
-	
+	wdg.setIndentation(0)
+
 	with avb.open(sys.argv[1]) as file_bin:
-		wdg.setModel(BinModel(file_bin.content))
+		wdg.setWindowTitle(sys.argv[1])
+
+		prx = QtCore.QSortFilterProxyModel()
+		prx.setSourceModel(BinModel(file_bin.content))
+		wdg.setModel(prx)
+
+		wdg.selectionModel().selectionChanged.connect(show_details)
+		wdg.setItemDelegateForColumn(0, BinItemDisplayDelegate())
+		wdg.setSortingEnabled(True)
 
 		for col in range(wdg.model().columnCount()):
 			header_data = wdg.model().headerData(col, QtCore.Qt.Orientation.Horizontal, role=QtCore.Qt.UserRole)
