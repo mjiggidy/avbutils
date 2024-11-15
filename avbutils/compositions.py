c8313369cb2a2c5851d72df0a23e512605ce1e55
mjordan <michael@glowingpixel.com>
2024-11-15 13:50:44 -0800

    Clip colors

--- avbutils/compositions.py
+++ avbutils/compositions.py
@@ -1,10 +1,10 @@
 import enum, avb, collections, dataclasses
 
-ClipColor = collections.namedtuple("ClipColor", "R G B")
+#ClipColor = collections.namedtuple("ClipColor", "R G B")
 
 # TODO: Replace the namedtuple with this
 @dataclasses.dataclass
-class ClipColorClass:
+class ClipColor:
 
 	r:int
 	"""16-bit Red Channel"""
@@ -23,7 +23,7 @@ class ClipColorClass:
 		return tuple([round(c/self.max_16b()*self.max_8b()) for c in self.as_rgb16()])
 	
 	@classmethod
-	def from_rgb8(cls, r:int, g:int, b:int) -> "ClipColorClass":
+	def from_rgb8(cls, r:int, g:int, b:int) -> "ClipColor":
 		"""ClipColor from 8-bit RGB values"""
 		
 		return cls(
@@ -39,45 +39,6 @@ class ClipColorClass:
 	def max_16b():
 		"""Maximum 16bit value"""
 		return (1 << 16) - 1
-
-
-def get_default_clip_colors() -> list[ClipColorClass]:
-	"""Default clip colors for top-level compositions (16-bit RGB triads)"""	
-	return [ClipColorClass(*x) for x in [
-		[64000,48640,48640],
-		[56832,25600,29696],
-		[65280,0,29440],
-		[48896,0,26112],
-		[32256,12544,26880],
-		[49408,19200,41216],
-		[61440,12800,58880],
-		[36608,0,45824],
-		[58880,48640,65280],
-		[22528,17920,58624],
-		[14592,11776,38144],
-		[15104,25344,37888],
-		[23040,38912,58112],
-		[16896,54272,62464],
-		[17920,39168,36864],
-		[43520,65280,49920],
-		[25856,50432,21248],
-		[16896,32768,13824],
-		[61952,65280,16384],
-		[32256,32768,14336],
-		[49408,50176,22016],
-		[56064,55296,47104],
-		[58368,50688,0],
-		[48896,43264,36608],
-		[65280,50176,32768],
-		[62720,33280,12544],
-		[32256,20992,13568],
-		[49664,32256,20992],
-		[48896,48896,48896],
-		[22784,22784,22784],
-		[32768,9216,9216],
-		[51200,14592,14592]
-	]]
-
 
 
 class MobTypes(enum.IntEnum):
@@ -130,6 +91,44 @@ def composition_is_toplevel(comp:avb.trackgroups.Composition) -> bool:
 	# NOTE: Using rules from `avb.bin.Bin.toplevel()``
 	return MobTypes.from_composition(comp) == MobTypes.COMPOSITION_MOB and MobUsage.from_composition(comp) == MobUsage.UNDEFINED
 
+
+def get_default_clip_colors() -> list[ClipColor]:
+	"""Default clip colors for top-level compositions (16-bit RGB triads)"""	
+	return [ClipColor(*x) for x in [
+		[64000,	48640,	48640],
+		[56832,	25600,	29696],
+		[65280,	0,		29440],
+		[48896,	0,		26112],
+		[32256,	12544,	26880],
+		[49408,	19200,	41216],
+		[61440,	12800,	58880],
+		[36608,	0,		45824],
+		[58880,	48640,	65280],
+		[22528,	17920,	58624],
+		[14592,	11776,	38144],
+		[15104,	25344,	37888],
+		[23040,	38912,	58112],
+		[16896,	54272,	62464],
+		[17920,	39168,	36864],
+		[43520,	65280,	49920],
+		[25856,	50432,	21248],
+		[16896,	32768,	13824],
+		[61952,	65280,	16384],
+		[32256,	32768,	14336],
+		[49408,	50176,	22016],
+		[56064,	55296,	47104],
+		[58368,	50688,	0    ],
+		[48896,	43264,	36608],
+		[65280,	50176,	32768],
+		[62720,	33280,	12544],
+		[32256,	20992,	13568],
+		[49664,	32256,	20992],
+		[48896,	48896,	48896],
+		[22784,	22784,	22784],
+		[32768,	9216,	9216 ],
+		[51200,	14592,	14592]
+	]]
+
 def composition_clip_color(comp:avb.trackgroups.Composition) -> tuple[int,int,int]|None:
 	"""Return the clip color of a composition if set"""
 
@@ -137,6 +136,6 @@ def composition_clip_color(comp:avb.trackgroups.Composition) -> tuple[int,int,in
 
 	attrs = comp.attributes
 	try:
-		return ClipColorClass(*(attrs[c] for c in color_attr_fields))
+		return ClipColor(*(attrs[c] for c in color_attr_fields))
 	except KeyError:
 		return None
