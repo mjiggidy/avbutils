64b8367c0fee65f8c58a5553f677c869986d1a93
Michael Jordan <michael@glowingpixel.com>
2024-12-03 12:09:48 -0800

    Begin migrate to view model

--- examples/marker_list.py
+++ examples/marker_list.py
@@ -2,6 +2,100 @@ import sys
 import avb, avbutils
 from PySide6 import QtCore, QtGui, QtWidgets
 
+class MarkerViewer(QtWidgets.QWidget):
+
+	def __init__(self):
+		super().__init__()
+
+		self.setLayout(QtWidgets.QVBoxLayout())
+
+		self.cmb_timelines   = QtWidgets.QComboBox()
+		self.view_markerlist = QtWidgets.QTreeView()
+
+		self.layout().addWidget(self.cmb_timelines)
+		self.layout().addWidget(self.view_markerlist)
+
+class MarkerViewItem(QtCore.QObject):
+	"""Marker view object for view model"""
+
+	HEADERS = [
+		"Timeline",
+		"Color",
+		"Timecode",
+		"Track",
+		"User",
+		"Comment",
+	]
+
+class MarkerViewModel(QtCore.QAbstractItemModel):
+	"""Marker view model"""
+
+	def __init__(self):
+		super().__init__()
+
+		self._marker_data:list[MarkerViewItem]
+
+	def columnCount(self, parent:QtCore.QModelIndex) -> int:
+		if parent.isValid():
+			return 0
+		return len(MarkerViewItem.HEADERS)
+	
+	def rowCount(self, parent:QtCore.QModelIndex) -> int:
+		if parent.isValid():
+			return 0
+		return len(self._marker_data)
+	
+	def parent(self, index:QtCore.QModelIndex) -> QtCore.QModelIndex:
+		return QtCore.QModelIndex()
+	
+	def headerData(self, section:int, orientation:QtCore.Qt.Orientation, role:QtCore.Qt.ItemDataRole):
+		if orientation is QtCore.Qt.Orientation.Horizontal:
+			return None
+		
+		if role is QtCore.Qt.ItemDataRole.DisplayRole:
+			return MarkerViewItem.HEADERS[section]
+	
+	def data(self, index:QtCore.QModelIndex):
+		"""TODO"""
+
+
+class AppController(QtCore.QObject):
+	"""Application controller"""
+
+	def __init__(self, initial_bin_path:str|None=None):
+		super().__init__()
+
+		self._viewmodel = MarkerViewModel()
+		self._bin_path:str = None
+
+		self._wnd_main = MarkerViewer()
+
+		if not initial_bin_path or not QtCore.QFileInfo(initial_bin_path).isFile():
+			self.promptForBin()
+		else:
+			self.setCurrentBin(initial_bin_path)
+
+		self._wnd_main.show()
+	
+	def promptForBin(self):
+		"""Prompt the user for a bin"""
+		bin_path, _ = QtWidgets.QFileDialog.getOpenFileName(self._wnd_main, "Choose An Avid Bin...", self._bin_path, "Avid Bins (*.avb);;All Files (*)")
+		if bin_path:
+			self.setCurrentBin(bin_path)
+
+
+	def setCurrentBin(self, bin_path:str):
+		"""Set the current Avid bin"""
+		self._bin_path = bin_path
+
+		self.loadBin()
+	
+	def loadBin(self):
+
+		with avb.open(self._bin_path) as bin_handle:
+			pass
+	
+
 class MarkerIcons:
 
 	ICON_MARGIN:int = 6
@@ -74,30 +168,6 @@ if __name__ == "__main__":
 
 	app = QtWidgets.QApplication(sys.argv)
 
-	wnd_main = QtWidgets.QWidget()
-
-	wnd_main.setLayout(QtWidgets.QVBoxLayout())
-	wnd_main.setWindowTitle("Marker List")
-	wnd_main.setMinimumWidth(600)
-
-	tree_markerlist = QtWidgets.QTreeWidget()
-	tree_markerlist.setHeaderLabels([
-		"Color",
-		"Timecode",
-		"Track",
-		"Author",
-		"Comment"
-	])
-	tree_markerlist.setAlternatingRowColors(True)
-	tree_markerlist.setSortingEnabled(True)
-	tree_markerlist.setUniformRowHeights(True)
-	tree_markerlist.setIndentation(0)
-	wnd_main.layout().addWidget(tree_markerlist)
-
-	wnd_main.show()
-
-	if len(sys.argv) > 1:
-		markers = load_markers_from_bin(sys.argv[1], tree_markerlist)
-		print(markers)
+	app_controller = AppController()
 
 	sys.exit(app.exec())
