2f5fa2caae2968c82a38d78cb348cd99e0c6a31f
Michael Jordan <michael@glowingpixel.com>
2025-08-29 16:27:24 -0700

    Ident and notes

--- examples/identification_exploration.py
+++ examples/identification_exploration.py
@@ -17,8 +17,8 @@ def composition_is_timeline(comp:avb.trackgroups.Composition) -> bool:
 
 def composition_is_masterclip(comp:avb.trackgroups.Composition) -> bool:
 	"""Composition is a regular masterclip"""
-	return avbutils.MobUsage.from_composition(comp) == avbutils.MobUsage.UNDEFINED \
-	   and avbutils.MobTypes.from_composition(comp) == avbutils.MobTypes.MASTER_MOB
+	return avbutils.MobTypes.from_composition(comp) == avbutils.MobTypes.MASTER_MOB \
+	   and avbutils.MobUsage.from_composition(comp) == avbutils.MobUsage.UNDEFINED
 
 # NOTE: I think we can just rely on usage code here
 def composition_is_master_mob(comp:avb.trackgroups.Composition) -> bool:
@@ -34,6 +34,27 @@ def composition_is_source_mob(comp:avb.trackgroups.Composition) -> bool:
 
 def composition_is_effect_mob(comp:avb.trackgroups.Composition) -> bool:
 	return avbutils.MobUsage.from_composition(comp) == avbutils.MobUsage.EFFECT
+
+def composition_is_groupclip(comp:avb.trackgroups.Composition) -> bool:
+	# MobType.COMPOSITION_MOB always, it seems
+	return avbutils.MobUsage.from_composition(comp) == avbutils.MobUsage.GROUP_CLIP
+
+def composition_is_groupoofter(comp:avb.trackgroups.Composition) -> bool:
+	# NOTE: GroupClip/GroupOofter == MasterClip/MasterMob?  Question mark?
+	# Seems to accompany GroupClips 1-to-1
+	# MobType.COMPOSITION_MOB always, it seems
+	return avbutils.MobUsage.from_composition(comp) == avbutils.MobUsage.GROUP_OOFTER
+
+def composition_is_motioneffect_mob(comp:avb.trackgroups.Composition) -> bool:
+	# NOTE: DID NOT FIND ANY YET
+	return avbutils.MobUsage.from_composition(comp) == avbutils.MobUsage.MOTION
+
+
+def composition_is_precompute(comp:avb.trackgroups.Composition) -> bool:
+	# NOTE: avbutils.MobUsage.PRECOMPUTE seems to pair with avbutils.MobTypes.MASTER_MOB.  So like a Precompute Clip
+	# Then avbutils.MobUsage.PRECOMPUTE_FILE,PRECOMPUTE_SOURCE_MOB source/file mobs pair with avbutils.MobTypes.SOURCE_MOB
+	# Need to verify
+	return avbutils.MobUsage.from_composition(comp) in [avbutils.MobUsage.PRECOMPUTE, avbutils.MobUsage.PRECOMPUTE_FILE, avbutils.MobUsage.PRECOMPUTE_SOURCE_MOB]
 
 
 
@@ -85,6 +106,32 @@ def get_effect_mobs_from_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool
 			continue
 		yield i.mob
 
+def get_groupclips_from_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False) -> typing.Generator[avb.trackgroups.Composition,None,None]:
+
+	for i in bin_content.items:
+		if not composition_is_groupclip(i.mob):
+			continue
+		if not allow_reference_clips and not i.user_placed:
+			continue
+		yield i.mob
+
+def get_motioneffects_from_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False) -> typing.Generator[avb.trackgroups.Composition,None,None]:
+
+	for i in bin_content.items:
+		if not composition_is_motioneffect_mob(i.mob):
+			continue
+		if not allow_reference_clips and not i.user_placed:
+			continue
+		yield i.mob
+
+def get_precomputes_from_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False) -> typing.Generator[avb.trackgroups.Composition,None,None]:
+
+	for i in bin_content.items:
+		if not composition_is_precompute(i.mob):
+			continue
+		if not allow_reference_clips and not i.user_placed:
+			continue
+		yield i.mob
 
 
 
@@ -95,7 +142,88 @@ def get_effect_mobs_from_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool
 
 
 
+def show_precomute_data_for_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False):
 
+	# NOTE:
+
+	clip_audio = []
+	clip_video = []
+	clip_other = []
+
+	for clip in get_precomputes_from_bin(bin_content, allow_reference_clips=allow_reference_clips):
+
+		print(clip)
+
+		track_types = avbutils.get_track_types_from_composition(clip)
+
+
+		if avbutils.TrackTypes.PICTURE in track_types:
+			print("Looks like a video", avbutils.MobUsage.from_composition(clip), avbutils.MobTypes.from_composition(clip))
+			clip_video.append(clip)
+		elif avbutils.TrackTypes.SOUND in track_types:
+			print("Looks like an audio", avbutils.MobUsage.from_composition(clip), avbutils.MobTypes.from_composition(clip))
+			clip_audio.append(clip)
+		else:
+			print("Weird one:", track_types)
+			clip_other.append(clip)
+		
+		print("--")
+
+	print(f"{len(clip_video)=}  {len(clip_audio)=}  {len(clip_other)=}")
+
+def show_motioneffect_data_for_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False):
+
+	clip_audio = []
+	clip_video = []
+	clip_other = []
+
+	for clip in get_motioneffects_from_bin(bin_content, allow_reference_clips=allow_reference_clips):
+
+		print(clip)
+
+		track_types = avbutils.get_track_types_from_composition(clip)
+
+
+		if avbutils.TrackTypes.PICTURE in track_types:
+			print("Looks like a video", clip.usage, clip.mob_type)
+			clip_video.append(clip)
+		elif avbutils.TrackTypes.SOUND in track_types:
+			print("Looks like an audio", clip.usage, clip.mob_type)
+			clip_audio.append(clip)
+		else:
+			print("Weird one:", track_types)
+			clip_other.append(clip)
+		
+		print("--")
+
+	print(f"{len(clip_video)=}  {len(clip_audio)=}  {len(clip_other)=}")
+
+def show_groupclip_data_for_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False):
+
+	clip_audio = []
+	clip_video = []
+	clip_other = []
+
+	for clip in get_groupclips_from_bin(bin_content, allow_reference_clips=allow_reference_clips):
+
+		print(clip)
+
+		track_types = avbutils.get_track_types_from_composition(clip)
+
+
+		if avbutils.TrackTypes.PICTURE in track_types:
+			print("Looks like a video", clip.usage, clip.mob_type)
+			clip_video.append(clip)
+		elif avbutils.TrackTypes.SOUND in track_types:
+			print("Looks like an audio", clip.usage, clip.mob_type)
+			clip_audio.append(clip)
+		else:
+			print("Weird one:", track_types)
+			clip_other.append(clip)
+		
+		print("--")
+
+	print(f"{len(clip_video)=}  {len(clip_audio)=}  {len(clip_other)=}")
 
 def show_effectmob_data_for_bin(bin_content:avb.bin.Bin, allow_reference_clips:bool=False):
 
@@ -186,6 +314,10 @@ def show_master_mob_data_for_bin(bin_content:avb.bin.Bin, allow_reference_clips:
 
 	usage = []
 	for clip in get_master_mobs_from_bin(bin_content, allow_reference_clips=allow_reference_clips):
+
+		# Skip regular ol' masterclips for now
+		if composition_is_masterclip(clip):
+			continue
 		
 		print(clip)
 		
@@ -258,7 +390,7 @@ def main(bin_path:os.PathLike):
 
 		#show_masterclip_data_for_bin(bin_handle.content, True)
 		
-		show_master_mob_data_for_bin(bin_handle.content, True)
+		show_precomute_data_for_bin(bin_handle.content, True)
 
 
 
